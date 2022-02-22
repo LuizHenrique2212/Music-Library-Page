412362e6cdaa54699d175dc3daa2569abe49dbe0
Luiz Henrique <luizhenrique2205@gmail.com>
2022-02-21 21:52:48 -0300

    mudanças no layout, correção de bug no botão que abre o menu

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 let openMenu = document.querySelector('.open-menu');
 let closeMenu = document.querySelector('.close-menu');
 let navBar =  document.querySelector('nav');
+let pageBackground = document.querySelector('.container');
 
 openMenu.onclick = function() {
    navBar.style.left = '0';
+   pageBackground.style.opacity = '0';
 };
 
 closeMenu.onclick = function(){
-    navBar.style.left = '-320px';
+    navBar.style.left = '-320px'
+    pageBackground.style.opacity = '1'
 }
