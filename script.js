3bf0c16746adccf8ea5e6ec398f179f16af4bc38
Luiz Henrique <luizhenrique2205@gmail.com>
2022-03-02 19:10:14 -0300

    correção do bug do menu que não somia por completo

--- script.js
+++ script.js
@@ -2,13 +2,16 @@ let openMenu = document.querySelector('.open-menu');
 let closeMenu = document.querySelector('.close-menu');
 let navBar =  document.querySelector('nav');
 let pageBackground = document.querySelector('.container');
+let mainDisplay = document.querySelector('.main-display');
 
 openMenu.onclick = function() {
-   navBar.style.left = '0';
-   pageBackground.style.opacity = '0';
+    navBar.style.left = '0';
+    navBar.style.zIndex = '100000';
+    mainDisplay.style.opacity = '0.3';
 };
 
 closeMenu.onclick = function(){
-    navBar.style.left = '-320px'
-    pageBackground.style.opacity = '1'
+    navBar.style.left = '-400px';
+    navBar.style.zIndex = '';
+    mainDisplay.style.opacity = '1';
 }
