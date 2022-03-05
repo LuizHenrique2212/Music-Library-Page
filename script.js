552804ceaa94ce1e87887cd68144c680b81869ef
Luiz Henrique <luizhenrique2205@gmail.com>
2022-03-05 11:17:08 -0300

    versão mobile corrigida

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
 let openMenu = document.querySelector('.open-menu');
 let closeMenu = document.querySelector('.close-menu');
-let navBar =  document.querySelector('nav');
-let pageBackground = document.querySelector('.container');
-let mainDisplay = document.querySelector('.main-display');
+let navBar = document.querySelector('nav');
 
-openMenu.onclick = function() {
-    navBar.style.left = '0';
-    navBar.style.zIndex = '100000';
-    mainDisplay.style.opacity = '0.3';
+openMenu.onclick = function () {
+    navBar.style.display = 'block';
+    navBar.style.zIndex = '1';
+    openMenu.style.display = 'none';
 };
 
-closeMenu.onclick = function(){
-    navBar.style.left = '-400px';
+closeMenu.onclick = function () {
+    navBar.style.display = 'none';
     navBar.style.zIndex = '';
-    mainDisplay.style.opacity = '1';
+    openMenu.style.display = '';
 }
